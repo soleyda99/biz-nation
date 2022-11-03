7588257d2a6c4a5587dcc8d8472f437481b5031e
Soleyda De Sousa <soleyda.desousa@lilab.pe>
2022-11-03 20:50:17 -0300

    subir a netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'df3a0b75dd30deee3496bc749f6f525b',
+  hash: '0676b28bf461087f176c699de9cdee4e',
+  apiUrl: 'https://gateway.marvel.com:443/v1/public/characters',
+  ts: '1',
 };
